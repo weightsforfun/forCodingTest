9a42caaf18856e20b3dd11747db8871078ccbcb8
weightsforfun <giyoung914@gmail.com>
2021-12-26 13:46:49 +0900

    week8 done

--- comprehensive-week8/4673.py
+++ comprehensive-week8/4673.py
@@ -1,2 +1,19 @@
-for i in range(100):
-    
+arr=[]
+
+checked=[0]*10001
+for i in range(0,10001):
+    arr.append(i)
+
+for i in range(1,10001):
+    num=i
+    for j in str(i):
+        num+=int(j)
+    if(num<=10000):
+        if(not checked[num]):
+            arr[num]=-1
+            checked[num]=1
+for i in arr:
+    if(i!=-1 and i!=0):
+        print(i)
+
+
