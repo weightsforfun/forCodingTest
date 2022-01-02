ae8bcdb9c37c9b954ac068f98581d803a137a72c
weightsforfun <giyoung914@gmail.com>
2022-01-02 15:25:57 +0900

    fixed 1874

--- comprehensive-week9/1874.py
+++ comprehensive-week9/1874.py
@@ -10,25 +10,22 @@ for i in range(n):
     arr.append(int(sys.stdin.readline()))
 i=1
 while(len(a1)<len(arr)):
-    if(len(a2)==0):
+    if(len(a2)==0):  
         a2.append(i)
         answer.append("+")
         i+=1
-    else:
-        if(arr[index]>a2[-1]):
-            a2.append(i)
+    else:   
+        if(arr[index]>a2[-1]):   #스택에 넣는수가 수열의 수보다 작을경우 계속 넣어준다
+            a2.append(i) 
             i+=1
             answer.append("+")
-        elif(arr[index]==a2[-1]):
+        elif(arr[index]==a2[-1]):    #스택의 마지막수와 수열수가 같다면 빼서 수열을 만들어 주는데ㅐ 쓴다
             a1.append(a2.pop())
             answer.append("-")
             index+=1
-        else:
-            a1.append(a2.pop())
-            answer.append("-")
-            index+=1
+        else:     ## 그외경우는 스택으로 못만듬
+            print("NO")
+            break
 if(arr==a1):
     for i in answer:
         print(i)
-else:
-    print("NO")
