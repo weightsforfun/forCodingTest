9a7851b1cf42e199384aeedf5cb8721b7f44d8e9
weightsforfun <giyoung914@gmail.com>
2022-02-23 10:43:28 +0900

    week 16 3/6 done

--- week16/11286.py
+++ week16/11286.py
@@ -8,8 +8,8 @@ for i in range(n):
     command=int(input())
     if(command==0):
         if(heap):
-            print(heapq.heappop(heap))
+            print(heapq.heappop(heap)[1])
         else:
             print(0)
     else:
-        heapq.heappush(heap,command)
+        heapq.heappush(heap,(abs(command),command))
