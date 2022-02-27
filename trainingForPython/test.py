b6e8c4b39087f4a6057f884da95e69ba2374a23b
weightsforfun <giyoung914@gmail.com>
2022-02-27 19:22:44 +0900

    week16 6/6 done but 1202 googled it

--- trainingForPython/test.py
+++ trainingForPython/test.py
@@ -1,20 +1,23 @@
-def check(num):
-    length = len(num)
-    for idx in range(1, length//2 + 1):
-        if num[-idx:] == num[-(idx*2):-idx]:
-            return False
-    else:
-        return True
-
-def recursive(num):
-    global N, res
-    if len(num) == N:
-        print(num)
-        exit()
-    for i in '123':
-        if check(num + str(i)):
-            recursive(num + str(i))
-    return
-
-N = int(input())
-recursive('1')
+def binary_search(target,array):
+    start=0
+    end=len(array)-1
+    while start<=end:
+        mid=(start+end)//2
+        if(array[mid][0]==target):
+            while(array[mid][1]==1):
+                mid+=1
+                if(mid==len(array)):
+                    return -1
+            return mid
+        elif(array[mid][0]<target):
+            start=mid+1
+        else:
+            end=mid-1
+    end+=1
+    while(array[end][1]==1):
+        end+=1
+        if(end==len(array)):
+            return -1
+    return end
+arr=[[2,0],[3,1],[5,1],[7,1],[9,1]]
+print(binary_search(3,arr))
--- week16/14889.py
+++ week16/14889.py
@@ -2,26 +2,26 @@ import sys
 from itertools import combinations
 input=sys.stdin.readline
 n=int(input())
-ability=[list(map(int,input().split(" "))) for _ in range(n)]
+statsArray=[list(map(int,input().split(" "))) for _ in range(n)]
 
 arr=[]
 for i in range(n):
     arr.append(i)
 
-comb=list(combinations(arr,n//2))
+TeamCaseCanBeMade=list(combinations(arr,n//2))
 
 def getSumOfStat(teamMember):  ## 조합으로 만든 팀 구성원 넣어주면 그 팀의 시너지값을 return함
-    global ability
+    global statsArray
     SumofStat=0
     for i in range(0,len(teamMember)):
         for j in range(i+1,len(teamMember)):  
-            SumofStat+=(ability[teamMember[i]][teamMember[j]]+ability[teamMember[j]][teamMember[i]])
+            SumofStat+=(statsArray[teamMember[i]][teamMember[j]]+statsArray[teamMember[j]][teamMember[i]])
     return SumofStat
 
 
 diff=1001  # 나올수있는 차의 최대값+1
-for i in range(len(comb)//2): ## 만들어진 모든 조합중 반만 해도 상태딤이 그 절반이니까 반만돌린다 i우리팀 -1-i반대편부터 상대팀으로 차이구해줌
-    diff=min(diff,abs(getSumOfStat(comb[i])-getSumOfStat(comb[-i-1])))
+for i in range(len(TeamCaseCanBeMade)//2): ## 만들어진 모든 조합중 반만 해도 상태딤이 그 절반이니까 반만돌린다 i우리팀 -1-i반대편부터 상대팀으로 차이구해줌
+    diff=min(diff,abs(getSumOfStat(TeamCaseCanBeMade[i])-getSumOfStat(TeamCaseCanBeMade[-i-1])))
    
 print(diff)
 ## 팀 누구 선택하냐에 따라 다 달라지기때문에 브루트포스라 생각함 예시도 너무 적었고 그다음부터는 그냥 구현 문제
