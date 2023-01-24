f259f26eb2a65d0e14f51a77fe054ef44bb0a239
weightsforfun <giyoung914@gmail.com>
2023-01-24 19:47:25 +0900

    programmers level3 dp

---
+++ programmers/level3/integerTriangle.py
@@ -0,0 +1,16 @@
+def solution(triangle):
+    answer = 0
+    triangle[1][0]+=triangle[0][0]
+    triangle[1][1]+=triangle[0][0]
+
+    for i in range(2,len(triangle)):
+        for j in range(len(triangle[i])):
+            if(j==0):
+                triangle[i][j]+=triangle[i-1][j]
+            elif(j==len(triangle[i])-1):
+                triangle[i][j]+=triangle[i-1][-1]
+            else:
+                triangle[i][j]+=max(triangle[i-1][j-1],triangle[i-1][j])
+
+    answer=max(triangle[-1])
+    return answer
