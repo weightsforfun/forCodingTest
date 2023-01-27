c30172e691209b6390a54f01b81c856e809b8ea3
weightsforfun <giyoung914@gmail.com>
2023-01-27 18:06:09 +0900

    programmers done

---
+++ programmers/level4/thiefdp.py
@@ -0,0 +1,33 @@
+
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+def solution(money):
+    answer = 0
+    dp=[0]*(len(money))
+    dp[0]=money[0]
+    dp[1]=max(dp[0],money[1])
+    for i in range(2,len(money)-1):
+        dp[i]=max(dp[i-2]+money[i],dp[i-1])
+
+    dp2=[-1]*(len(money))
+    dp2[0]=0
+    dp2[1]=money[1]
+    for i in range(2,len(money)):
+        dp2[i]=max(dp2[i-2]+money[i],dp2[i-1])
+
+    answer=max(dp[-2],dp2[-1])
+    return answer
