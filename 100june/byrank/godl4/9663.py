6042b082f0353db12f91456fbdb6362ca9dbc532
weightsforfun <giyoung914@gmail.com>
2023-02-07 11:01:38 +0900

    nqueen dine

--- 100june/byrank/godl4/9663.py
+++ 100june/byrank/godl4/9663.py
@@ -5,7 +5,9 @@ answer=0
 
 def check(x):
     for i in range(x):
-        if(arr[x]==arr[i] or abs())
+        if(arr[x]==arr[i] or abs(x-i)==abs(arr[x]-arr[i])):
+            return False
+    return True
 
 def dfs(row):
     global answer
@@ -16,44 +18,11 @@ def dfs(row):
         return
     
     for i in range(n):
-        if(arr[row][i]==0):
-            #세로 0으로 만들기
-            for j in range(row,n):
-                arr[j][i]=1
-            # 기울기가 양인 대각선
-            temp_row=row
-            temp_column=i
-            while( 0<=temp_row<n and 0<=temp_column<n):
-                arr[temp_row][temp_column]=1
-                temp_row+=1
-                temp_column-=1
-            #기울기가 음인 대각선
-            temp_row=row
-            temp_column=i
-            while( 0<=temp_row<n and 0<=temp_column<n):
-                arr[temp_row][temp_column]=1
-                temp_row+=1
-                temp_column+=1
-            
+        arr[row]=i
+        if(check(row)):
             dfs(row+1)
-            
-            for j in range(row,n):
-                arr[j][i]=0
-            # 기울기가 양인 대각선
-            temp_row=row
-            temp_column=i
-            while( 0<=temp_row<n and 0<=temp_column<n):
-                arr[temp_row][temp_column]=0
-                temp_row+=1
-                temp_column-=1
-            #기울기가 음인 대각선
-            temp_row=row
-            temp_column=i
-            while( 0<=temp_row<n and 0<=temp_column<n):
-                arr[temp_row][temp_column]=0
-                temp_row+=1
-                temp_column+=1
         
-dfs(1)
+        
+dfs(0)
 print(answer)
     
