4abae8d0a73746883c3d21412aaa948f027c757e
weightsforfun <giyoung914@gmail.com>
2023-03-09 22:11:10 +0900

    dynamic with bit masking

--- 100june/byrank/gold3/14391.py
+++ 100june/byrank/gold3/14391.py
@@ -1,19 +1,44 @@
-n,m=map(int,input().split())
-arr=[]
-for i in range(n):
-    arr.append(list(map(int,list(input()))))
+def bitmask():
+    global maxAns
+    # 비트마스크로 2^(N*M)의 경우의 수를 따져본다
+    for i in range(1 << n * m):
+        total = 0
+        # 가로 합 계산
+        for row in range(n):
+            rowsum = 0
+            for col in range(m):
+                # idx 는 이차원 배열을 일렬로 늘렸을때의 인덱스가 어디인지 의미
+                idx = row * m + col
+                # 가로일때
+                if i & (1 << idx) != 0:
+                    rowsum = rowsum * 10 + arr[row][col]
+                # 세로일때 앞에서 나온 수를 total에 더하고 rowsum 초기화
+                else:
+                    total += rowsum
+                    rowsum = 0
+            total += rowsum
 
-a1=0
-a2=0
-for i in range(n):
-    num=10**(m-1)
-    for j in range(m):
-        a1+=arr[i][j]*num
-        num//=10
+        # 세로 합 계산
+        for col in range(m):
+            colsum = 0
+            for row in range(n):
+                # idx 는 이차원 배열을 일렬로 늘렸을때의 인덱스가 어디인지 의미
+                idx = row * m + col
+                # 세로일때
+                if i & (1 << idx) == 0:
+                    colsum = colsum * 10 + arr[row][col]
+                # 가로일때 앞에서 나온 수를 total에 더하고 colsum 초기화
+                else:
+                    total += colsum
+                    colsum = 0
+            total += colsum
+        maxAns = max(maxAns, total)
 
-for i in range(m):
-    num=10**(n-1)
-    for j in range(n):
-         a2+=arr[j][i]*num
-         num//=10
-print(max(a1,a2))
+
+n, m = map(int, input().split())
+
+arr = [list(map(int, input())) for _ in range(n)]
+
+maxAns = 0
+bitmask()
+print(maxAns)
