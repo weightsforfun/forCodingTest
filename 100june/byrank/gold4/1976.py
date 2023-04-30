1946e4597df157b94e6102c33f9dbd5e567bb07f
weightsforfun <giyoung914@gmail.com>
2023-04-30 23:04:52 +0900

    union find need to be fixed

---
+++ 100june/byrank/gold4/1976.py
@@ -0,0 +1,42 @@
+n=int(input())
+m=int(input())
+arr=[]
+for i in range(n):
+    arr.append(list(map(int,input().split())))
+    
+parent=[0]*n
+
+for i in range(n):
+    parent[i]=i
+
+def find(x):
+    if parent[x] != x:
+        parent[x] = find(parent[x])
+    return parent[x]
+
+def merge(u,v):
+    u=find(u) 
+    v=find(v)
+    if(u!=v):
+        if(u<v):
+            parent[u]=v
+        else:
+            parent[v]=u
+
+for i in range(n):
+    for j in range(n):
+        if(arr[i][j]==1):
+            merge(i,j)
+            print(parent)
+
+route=list(map(int,input().split()))
+fail=0
+
+for i in range(len(route)-1):
+    if(parent[route[i]-1]!=parent[route[i+1]-1]):
+        fail=1
+        print("NO")
+        break
+if(not fail):
+    print("YES")
+    
--- trainingForPython/osassi1.py
+++ trainingForPython/osassi1.py
@@ -1,44 +1,8 @@
-from collections import deque
+import seaborn as sns
+import matplotlib.pyplot  as plt
 
 
 
-arr=[7,0,1,2,0,3,0,4,2,3,0,3,2,1,2,0,1,7,0,1]
-
-
-def Opr(size,page_and_refbit):
-    SIZE=size
-    memory=deque()
-    faults=0
-    
-    for i in range(len(page_and_refbit)):
-        flag=0
-        is_there_value=0
-        index=-1
-        for j in memory:
-            if(page_and_refbit[i] == j[0]):
-                is_there_value=1
-            index+=1
-        if not is_there_value and len(memory)<SIZE:
-            memory.append([page_and_refbit[i],-1])
-            faults+=1
-            print(memory)
-        elif not is_there_value and len(memory)==SIZE:
-            for j in range(3):
-                if(memory[j][1]==-2):
-                    memory.popleft()
-                    flag=1
-                    break
-                else:
-                    memory[j][1]=-2
-                    memory.append(memory.popleft())
-            if(not flag):
-                memory.popleft()
-            memory.append([page_and_refbit[i],-1])
-            faults+=1
-            print(memory)
-        elif is_there_value:
-            memory[index][1]=-1
-                
-            print("hit")
-    return faults
-print(Opr(3,arr))
+tips = sns.load_dataset("tips")
+sns.boxplot(x="size", y="total_bill", hue="day", data=tips)
+plt.show()
