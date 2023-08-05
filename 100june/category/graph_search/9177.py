f85eb85ed92217d73093ba3b6d0bf7289db4a1b0
weightsforfun <giyoung914@gmail.com>
2023-08-05 18:26:35 +0900

    graph search 1 solved by python

--- 100june/category/graph_search/9177.py
+++ 100june/category/graph_search/9177.py
@@ -1,37 +1,34 @@
 import sys
-
+from collections import deque
 def input():
-    return sys.stdin.readline()
-flag=0
-def dfs(a,b,c,a_index,b_index,c_index):
-    
-    global flag
-    if(c_index==len(c)-1):
-        flag=1
-        return
-    if(flag==1):
-        return 
-    # print(a[a_index],b[b_index],c[c_index])
-     
-    if(a[a_index]==c[c_index] and b[b_index]!=c[c_index]):
-        dfs(a,b,c,a_index+1,b_index,c_index+1)
-    elif(a[a_index]!=c[c_index] and b[b_index]==c[c_index]):
-        dfs(a,b,c,a_index,b_index+1,c_index+1)
-    elif(a[a_index]==c[c_index] and b[b_index]==c[c_index]):
-        dfs(a,b,c,a_index+1,b_index,c_index+1)
-        dfs(a,b,c,a_index,b_index+1,c_index+1)
-    
+    return sys.stdin.readline().strip()
 
 n=int(input())
 
-
 for i in range(n):
     a,b,c=input().split(" ")
+    a+="@"
+    b+="@"
+    deq=deque()
+    deq.append([0,0,0])
     flag=0
-    a=a+"@"
-    b=b+"@"
-    dfs(a,b,c,0,0,0)
-    if(flag):
-        print("Data Set ",i+1,": Yes")
+    visited=[[0]*(len(b)) for _ in range(len(a))]
+    while(deq):
+        la,lb,lc=deq.popleft()
+        temp_check=0
+        if(lc==len(c)):
+            flag=1
+            deq.clear()
+            break
+        if(la<len(a) and a[la]==c[lc] and visited[la][lb]==0):
+            visited[la][lb]=1
+            temp_check=1
+            deq.append([la+1,lb,lc+1])
+        if(lb<len(b) and b[lb]==c[lc] and (visited[la][lb]==0 or temp_check==1)):
+            visited[la][lb]=1
+            deq.append([la,lb+1,lc+1])
+    if(flag==1):
+        print("Data set %d: yes" %(i+1))
     else:
-        print("Data Set ",i+1,": No")
+        print("Data set %d: no" %(i+1))
+    
